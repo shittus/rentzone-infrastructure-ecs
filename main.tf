cb6f2fea68d9ceba894407d6f25afba6b539e46d
soliu <soliu@yahoo.com>
2024-10-11 12:33:27 -0500

    vpc updated

--- main.tf
+++ main.tf
@@ -1,13 +1,22 @@
 locals {
-  region = var.region
+  region       = var.region
   project_name = var.project_name
-  environment = var.environment
+  environment  = var.environment
 }
 
 # create vpc module
 module "vpc" {
-    source = "git@github.com:shittus/terraform-modules.git//vpc"
+  source                       = "git@github.com:shittus/terraform-modules.git//vpc"
+  region                       = local.region
+  project_name                 = local.project_name
+  environment                  = local.environment
+  vpc_cidr                     = var.vpc_cidr
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
 
-    
-  
+
 }
