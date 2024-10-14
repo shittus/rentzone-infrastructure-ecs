bcd809f62e22cc54a2f395e21be304ca619c6cd5
soliu <soliu@yahoo.com>
2024-10-14 18:58:15 -0500

    updated

--- main.tf
+++ main.tf
@@ -75,3 +75,17 @@ module "ssl_certificate" {
   alternative_names = var.alternative_names
 }
 
+# Application Load balancer
+  module "alb" {
+    source = "https://github.com/shittus/terraform-modules.git//alb"
+    project_name = var.project_name
+    environment = var.environment
+    alb_security_group_id = module.security-group.alb_security_group_id
+    public_subnet_az1_id = module.vpc.public_subnet_az1_id
+    public_subnet_az2_id =module.vpc.ublic_subnet_az2_id
+    target_type = var.target_type
+    vpc_id = module.vpc.vpc_id
+    validated_certificate_arn = module.ssl_certificate.validated_certificate_arn
+    
+  }
+
